2c3396cd7715f868c22aa8a394f544b476802e70
hbtpoprock <gunnuu@hotmail.com>
2025-03-07 07:12:03 +0700

    make it collapsible

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,48 @@
 "use client";
 
-import { Layout } from "antd";
+import { Layout, Button, Menu } from "antd";
+import { MenuOutlined, CloseOutlined } from "@ant-design/icons";
 import UserProfile from "../components/UserProfile";
 import SearchUsers from "../components/SearchUsers";
+import { useState } from "react";
 
 const { Sider, Content } = Layout;
 
 export default function Dashboard() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setCollapsed(!collapsed);
+  };
+  console.log("collapsed", collapsed);
   return (
     <Layout style={{ minHeight: "100vh" }}>
       <Sider
-        width="20%"
+        width={300}
         breakpoint="lg"
         collapsedWidth="0"
+        collapsible
+        collapsed={collapsed}
+        onCollapse={setCollapsed}
         style={{
           background: "#fff",
           borderRight: "1px solid #ccc",
         }}
       >
+        <div style={{ padding: "16px", textAlign: "right" }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuOutlined /> : <CloseOutlined />}
+            onClick={toggleSidebar}
+          />
+        </div>
         <UserProfile />
       </Sider>
-      <Content style={{ padding: "20px" }}>
-        <SearchUsers />
-      </Content>
+      <Layout>
+        <Content style={{ padding: "20px" }}>
+          <SearchUsers />
+        </Content>
+      </Layout>
     </Layout>
   );
 }
